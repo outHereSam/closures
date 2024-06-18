d20c77c20412d52cfffc30090bddadca0d6ca76f
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-18 11:26:27 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const Person = {
+  name: "Tommy",
+  age: 32,
+  greet: function () {
+    console.log(
+      `Hello, my name is ${this.name} and I'm ${this.age} years old.`
+    );
+  },
+};
+
+const newPerson = {
+  name: "Abbie",
+  age: 25,
+};
+
+const anotherPerson = {
+  name: "Kofi",
+  age: 19,
+};
+
+const callNewPerson = Person.greet.call(newPerson);
+const callAnotherPerson = Person.greet.call(anotherPerson);
+
+// console.log(callNewPerson);
+// console.log(callAnotherPerson);
